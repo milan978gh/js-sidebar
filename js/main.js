c214482d8508fcfa6fee45aaf2d25a7036cf1ee7
milan978gh <amilandev78@gmail.com>
2020-12-31 13:24:25 +0100

    update

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const sidebarBtn = document.querySelector('.sidebar-toggle');
+const sidebar = document.querySelector('.sidebar');
+const closeBtn = document.querySelector('.close-btn');
+
+
+sidebarBtn.addEventListener('click', () => {
+  // if (sidebar.classList.contains('show-sidebar')) {
+  //   sidebar.classList.remove('show-sidebar');
+  // } else {
+  //   sidebar.classList.add('show-sidebar');
+  // }
+  sidebar.classList.toggle('show-sidebar');
+});
+
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
+});
